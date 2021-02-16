861c7295700083a2b2cd4b96b0045cc787280bd7
Tiago Andre <tandrezone@gmail.com>
2021-02-16 15:11:34 +0000

    Refractoring

--- app/Commands/ReadInfo.php
+++ app/Commands/ReadInfo.php
@@ -3,7 +3,7 @@
 namespace App\Commands;
 
 use App\Services\Utils;
-use Illuminate\Console\Scheduling\Schedule;
+use Carbon\Carbon;
 use LaravelZero\Framework\Commands\Command;
 
 
@@ -21,43 +21,45 @@ class ReadInfo extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Get number of cores, clock speed and temps';
+
+    /**
+     * Service Utils
+     * @var Utils
+     */
+    protected $utils;
+
+    public function __construct(Utils $utils)
+    {
+        $this->utils = $utils;
+        parent::__construct();
+    }
 
     /**
      * Execute the console command.
      *
-     * @return mixed
      */
     public function handle()
     {
+        $data = [];
 
-        $cpusSpeed = Utils::getClockSpeed();
-            $temp = Utils::getTemp();
+        $cpusSpeed = $this->utils->getClockSpeed();
+        $temp = $this->utils->getTemp();
 
-            $headers = ['CPU0', 'CPU1', 'CPU2', 'CPU3', 'TEMP'];
+        $headers = ['TIME', 'CPUS', 'CPU0', 'CPU1', 'CPU2', 'CPU3', 'TEMP'];
 
-            $data = [
-                [
-                    'CPU0' => $cpusSpeed[0],
-                    'CPU1' => $cpusSpeed[1],
-                    'CPU2' => $cpusSpeed[2],
-                    'CPU3' => $cpusSpeed[3],
-                    'TEMP' => $temp,
-                ],
+        $data[] =
+            [
+                'TIME' => (Carbon::now()->toTimeString()),
+                'CORES' => $this->utils->getNumberCpus(),
+                'CPU0' => $cpusSpeed[0],
+                'CPU1' => $cpusSpeed[1],
+                'CPU2' => $cpusSpeed[2],
+                'CPU3' => $cpusSpeed[3],
+                'TEMP' => $temp,
 
-            ];
+
+        ];
            $this->table($headers, $data);
-return 0;
-    }
-
-    /**
-     * Define the command's schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
     }
 }
--- app/Services/Utils.php
+++ app/Services/Utils.php
@@ -7,14 +7,19 @@ namespace App\Services;
 class Utils
 {
     /**
+     * Get the temp of the soc in C
      * @return int
      */
-    public static function getTemp() : int {
+    public function getTemp() : int {
         $data = self::getInfoFromFile(config("rpi.temps"));
         return round((int)$data/1000);
     }
 
-    public static function getClockSpeed() : array {
+    /**
+     * Get the clock speed of each one of the 4 cores
+     * @return int[]
+     */
+    public function getClockSpeed() : array {
         $cpu0 = self::getInfoFromFile(config("rpi.clockspeed0"));
         $cpu1 = self::getInfoFromFile(config("rpi.clockspeed1"));
         $cpu2 = self::getInfoFromFile(config("rpi.clockspeed2"));
@@ -27,15 +32,39 @@ class Utils
             ];
     }
 
-    private static function getInfoFromFile(string $path) {
+    /**
+     * Helper to get info from external file or app
+     * @param string $path
+     * @return string
+     */
+    private function getInfoFromFile(string $path) : string {
         $file = fopen($path,"r");
         $data = fgets($file);
         fclose($file);
         return $data;
     }
 
-    private static function roundToNM($data) {
+    /**
+     * Round the temp and the clock speed to mhz and C
+     * @param $data
+     * @return int
+     */
+    private function roundToNM($data) : int {
         return round((int)$data/1000);
+    }
+
+    /**
+     * Get the number of cores
+     * @return int
+     */
+    public function getNumberCpus() : int {
+        $ncpu = 1;
+        if(is_file('/proc/cpuinfo')) {
+            $cpuinfo = file_get_contents('/proc/cpuinfo');
+            preg_match_all('/^processor/m', $cpuinfo, $matches);
+            $ncpu = count($matches[0]);
+        }
+        return $ncpu;
     }
 
 
